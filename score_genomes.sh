95aee4be53a1a0732a06c74b4fb06cdfe72c03b2
eead-csic-compbio <compbio@eead.csic.es>
2017-06-21 17:58:51 +0200

    added user-provided inputdir

--- score_genomes.sh
+++ score_genomes.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] 
+  then 
+    echo "# Need the name of folder containing .faa genome files";
+    exit;
+fi
+
+inputdir=$1
+
 # 1) Get the Pfam domain composition of proteins
 # encoded in test genomes 
 
-for i in test/*.faa; do \
+for i in $inputdir/*.faa; do \
   if [ ! -f $i.out.hmmsearch.tab ]; then \
     type hmmsearch >/dev/null 2>&1 || { echo >&2 "# hmmsearch not found, please install"; exit 1; }
 
@@ -16,7 +24,7 @@ echo "Domain composition done"
 
 # 2) Get their Sulfur Score   
 
-for file in test/*.tab; do \
+for file in $inputdir/*.tab; do \
   perl scripts/pfam_score.pl -input $file \
   -matrixdir  data/entropies_matrix -size 500 > $file.score; \
   echo $file.score; tail $file.score; \
