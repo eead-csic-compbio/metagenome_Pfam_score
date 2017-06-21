2d26f10d30c6d7ea406052fd731ed62e8f5b0a5b
eead-csic-compbio <compbio@eead.csic.es>
2017-06-21 17:53:18 +0200

    renamed

---
+++ score_genomes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 1) Get the Pfam domain composition of proteins
+# encoded in test genomes 
+
+for i in test/*.faa; do \
+  if [ ! -f $i.out.hmmsearch.tab ]; then \
+    type hmmsearch >/dev/null 2>&1 || { echo >&2 "# hmmsearch not found, please install"; exit 1; }
+
+    hmmsearch  --cut_ga -o /dev/null --tblout \
+      $i.out.hmmsearch.tab data/my_Pfam.sulfur.hmm $i; \
+  fi
+done  
+
+echo "Domain composition done" 
+
+# 2) Get their Sulfur Score   
+
+for file in test/*.tab; do \
+  perl scripts/pfam_score.pl -input $file \
+  -matrixdir  data/entropies_matrix -size 500 > $file.score; \
+  echo $file.score; tail $file.score; \
+done
+
+
+
+
+
+
+
