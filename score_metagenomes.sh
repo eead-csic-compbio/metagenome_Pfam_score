8fdf1bc9abfc7bb6c55f548e5d4d16f36406c78a
valdeanda <valdeanda@ciencias.unam.mx>
2017-08-10 14:00:54 +0200

    description extended

--- score_metagenomes.sh
+++ score_metagenomes.sh
@@ -7,53 +7,56 @@ if [ $# -eq 0 ]
 fi
 
 inputdir=$1
-
+pathway="Sulfur cyle"
 datadir=sulfur_data_test
 
-echo "------------------------------------------------------------------"
-echo "          Computing Mean Size legth (MSL) of input metagenomes"
-echo "------------------------------------------------------------------"
 
 for i in $inputdir/*.faa; do \
   
   #1) Get the Mean Size Length of peptides encoded in metagenome
-  echo $i
+  
+echo "#Computing Mean Size Length (MSL) of $i...." 
   perl -lne 'if(/^(>.*)/){$h=$1}else{$fa{$h}.=$_} END{ foreach $h (keys(%fa)){$m+=length($fa{$h})}; printf("MSL = %1.0f\n",$m/scalar(keys(%fa))) }' $i > $i.msl
   cat $i.msl
-  echo "Find out appropiate fragment size of classifiere (genF)"
  
   # 2) Find out appropriate fragment size of classifier (genF)
   perl -lne 'BEGIN{@bins=(30,60,100,150,200,250,300);@th=(45,80,125,175,225,275,300)} if(/^MSL = (\S+)/){ $msl=$1; foreach $i (0 .. $#th){ if($msl<=$th[$i]){ print "genF = $bins[$i]"; exit } } }' $i.msl > $i.genF
   cat $i.genF
   #3) Get the Pfam domain composition of metagenomic peptides
-  if [ ! -f $i.out.hmmsearch.tab ]; then \
+   echo "# Annotating $pathway Pfam domains in $i ..."  
+   if [ ! -f $i.out.hmmsearch.tab ]; then \
     type hmmsearch >/dev/null 2>&1 || { echo >&2 "# hmmsearch not found, please install"; exit 1; }
 
     hmmsearch  --cut_ga -o /dev/null --tblout \
       $i.out.hmmsearch.tab $datadir/my_Pfam.sulfur.hmm $i; \
   fi
-  
+echo "$pathway domain composition done"
+echo 
   #4) Get the Sulfur Score specifying the MSL of your input metagenome 
   
   genF=`perl -lne 'if(/genF = (\S+)/){ print $1 }' $i.genF`
   perl scripts/pfam_score.pl -input $i.out.hmmsearch.tab \
     -size $genF -entropyfile $datadir/entropies_matrix_entropies.tab > $i.out.hmmsearch.tab.score
-  echo "------------------------------------------------------------------"  
-  echo "                 Computing MEBS final Score" 
-  echo "------------------------------------------------------------------"
-  #echo $i.out.hmmsearch.tab.score
-  #echo "..."
   MEBS_Score=`grep "Pfam entropy score" $i.out.hmmsearch.tab.score`;
-  echo "The MEBS  Score of  $i"
-  echo "is = $MEBS_Score"
-  echo "Done with $i"
-  echo "------------------------------------------------------------------"
+  echo "# $i"
+  echo "$MEBS_Score"
+  done
 
-done
-
- echo "Done computing MEBS final score"
+ echo "MEBS final Score done"
  echo "Thanks for using MEBS" 
-
+echo "-----------------------------------------------------------------"
+echo "NOTE: According to our $pathway benchmarks, depending on your MSL"
+echo "input metagenomet, the maximum expected Scores (MES) and the selected "
+echo "cutoff Scores (95th) are"
+echo
+echo "	GenF	MES	95th"
+echo "	30	7.762	7.66 "
+echo "	60	10.085	9.70"
+echo "	100	8.986	8.81"
+echo "	150	9.399	8.51"
+echo "	200	9.487	8.18"
+echo "	250	9.528	8.98"
+echo "	300	9.464	7.61"
 
 
 
