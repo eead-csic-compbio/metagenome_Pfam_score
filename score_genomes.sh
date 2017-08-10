c8acedbc87dfdd0e314566068ebf9c2e5c9b3a3c
eead-csic-compbio <compbio@eead.csic.es>
2017-08-10 13:17:47 +0200

    added documentation

--- score_genomes.sh
+++ score_genomes.sh
@@ -7,38 +7,37 @@ if [ $# -eq 0 ]
 fi
 
 inputdir=$1
-
+pathway="Sulfur cycle"
 datadir=sulfur_data_test
+cutoff=8.705
+maxscore=9.491
 
 # 1) Get the Pfam domain composition of proteins
-# encoded in test genomes 
-
 for i in $inputdir/*.faa; do \
+  echo "# Annotating $pathway Pfam domains in $i ..."
   if [ ! -f $i.out.hmmsearch.tab ]; then \
     type hmmsearch >/dev/null 2>&1 || { echo >&2 "# hmmsearch not found, please install"; exit 1; }
     hmmsearch  --cut_ga -o /dev/null --tblout \
       $i.out.hmmsearch.tab $datadir/my_Pfam.sulfur.hmm $i; \
   fi
+done
 
-echo "                    Sulfur domain composition done" 
-echo "------------------------------------------------------------------"
+echo "$pathway domain composition done"
+echo 
+
 # 2) Get their Sulfur Score   
-echo "                    Computing MEBS final Score" 
-echo "------------------------------------------------------------------"
+echo "Computing Multigenomic Entropy-Based Score (MEBS) final score ..." 
 for file in $inputdir/*.tab; do \
   perl scripts/pfam_score.pl -input $file \
   -entropyfile $datadir/entropies_matrix_entropies.tab -size real > $file.score; \
-MEBS_Score=`grep "Pfam entropy score" $file.score`
-#  echo $file.score; grep "Pfam entropy score" $file.score; \
-echo "The MEBS Score of"
-echo "your $file"
-echo "is = $MEBS_Score"
-echo "Done"
-echo "------------------------------------------------------------------"
+  MEBS_Score=`grep "Pfam entropy score" $file.score`
+  echo "# $file" 
+  echo $MEBS_Score 
 done
-echo "                    MEBS final Score done"
-echo "------------------------------------------------------------------"
-echo "NOTE: According to our benchmark, a Score >8.705 the most likely"
-echo "that your genome is closely involved in the Sulfur cycle" 
-echo "The highest  SS value you can get is 9.491" 
+
+echo "MEBS final Score done"
+echo 
+echo "NOTE: According to our $pathway benchmarks, a score > $cutoff indicates"
+echo "that your genome is most likely involved in the $pathway . " 
+echo "The maximum expcted $pathway score is $maxscore ." 
 echo "Thanks for using MEBS" 
